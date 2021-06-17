c45ed5568a08bcbc411b7fdef75e3e6e16ebd307
Kausinho <kausinhobrunow@gmail.com>
2021-06-17 19:33:16 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Página de games</title>
+</head>
+<body>
+    
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href=""><img src="images/logo.png" /></a>
+            </div><!--logo-->
+
+            <div class="menu">
+                <a href="">Sobre</a>
+                <a href="">Depoimentos</a>
+                <a class="btn-cta" href="">Baixar Agora!</a>
+            </div><!--menu-->
+            <div class="clear"></div><!--clear-->
+        </div><!--container-->
+    </header>
+
+    <section class="topo">
+        <div class="container">
+            <div class="texto-topo">
+                <h2>Desafie seus amigos nesse multiplayer repleto de aventuras.</h2>
+                <p>Magna ut consectetur dolor nulla ea elit enim non proident ea consectetur.Exercitation sunt et esse nulla officia magna ullamco non nostrud sunt ex et magna.</p>
+                <br/>
+                <a href=""><img src="images/appstore.png" /></a>
+                <a href=""><img src="images/googleplay.png" /></a>
+            </div><!--texto-topo-->
+            <div class="img-topo">
+                <img src="images/hero.jpg" />
+            </div><!--img-topo-->
+            <div class="clear"></div><!--clear-->
+        </div><!--container-->
+    </section><!--topo-->
+
+    <section class="depoimentos">
+        <div class="container">
+            <div class="depoimento-single">
+                <p>"O melhor jogo do ano! Incrível como consigo aprender e me divertir ao mesmo tempo!"</p>
+                <br/>
+                <span>Guilherme Grillo - Danki Code</span>
+            </div><!--depooimento--single-->
+
+            <div class="depoimento-single">
+                <p>"O melhor jogo do ano! Incrível como consigo aprender e me divertir ao mesmo tempo!"</p>
+                <br/>
+                <span>Klaussio Brunow - E&L</span>
+            </div><!--depooimento--single-->
+        </div><!--container-->
+    </section><!--depoimentos-->
+
+    <section class="sobre">
+        <div class="container">
+            <div class="titulo-sobre">
+                <h2>Sobre</h2>
+            </div><!--titulo-sobre-->
+
+            <div class="sobre-single">
+                <div class="titulo-single">
+                    <i class="fa fa-user"></i>
+                    <h3>Multiplayer</h3>
+                </div><!--titulo-single-->
+                <div class="sobre-conteudo">
+                    <p>Divirta-se com seus amigos e veja quem é o melhor da rodada!</p>
+                </div><!--sobre-conteudo-->
+                <div class="clear"></div><!--clear-->
+            </div><!--sobre-single-->
+
+            <div class="sobre-single">
+                <div class="titulo-single">
+                    <i class="fa fa-user"></i>
+                    <h3>Multiplayer</h3>
+                </div><!--titulo-single-->
+                <div class="sobre-conteudo">
+                    <p>Divirta-se com seus amigos e veja quem é o melhor da rodada!</p>
+                </div><!--sobre-conteudo-->
+                <div class="clear"></div><!--clear-->
+            </div><!--sobre-single-->
+
+            <div class="sobre-single">
+                <div class="titulo-single">
+                    <i class="fa fa-user"></i>
+                    <h3>Multiplayer</h3>
+                </div><!--titulo-single-->
+                <div class="sobre-conteudo">
+                    <p>Divirta-se com seus amigos e veja quem é o melhor da rodada!</p>
+                </div><!--sobre-conteudo-->
+                <div class="clear"></div><!--clear-->
+            </div><!--sobre-single-->
+            <div class="clear"></div><!--clear-->
+        </div><!--container-->
+    </section><!--sobre-->
+
+    <footer>
+        <div class="container">
+            <a href=""><i class="fab fa-instagram"></i></a>
+            <a href=""><i class="fab fa-facebook-f"></i></a>
+            <a href=""><i class="fab fa-youtube"></i></a>
+        </div><!--container-->
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="js/file.js"></script>
+</body>
+</html>
